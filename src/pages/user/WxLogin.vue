e00e39970cbbc9aed9bc829890f16c8d66092e59
婉儿 <stwaner@126.com>
2020-01-17 17:48:46 +0800

    add-wxlogin

--- src/pages/user/WxLogin.vue
+++ src/pages/user/WxLogin.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="wx-login">
-    <p>微信登录</p>
-    <img src="../../assets/img/succ-icon.png" alt="微信二维码">
+    <p class="title">微信登录</p>
+    <div class="wx-pic">
+      <img class="wx-img" src="" alt="微信二维码">
+    </div>
     <div class="tip">
-      <p>请使用微信扫描二维码登录<br/>“蓝桥网”</p>
+      <img src="~@/assets/img/tip-img.png" alt="扫描登录">
     </div>
   </div>
 </template>
 
 <script>
-import LoginForm from '../../components/Login.vue'
 export default {
   name: 'WxLogin',
   data (){
     return{
     }
   },
-  components:{LoginForm}
 }
 </script>
 
@@ -27,6 +27,31 @@ export default {
   color: #fff;
   text-align: center;
   background:rgba(51,51,51,1);
+  .title{
+    font-size:20px;
+    font-family:PingFangSC-Medium,PingFang SC;
+    font-weight:bold;
+    color:rgba(255,255,255,1);
+    line-height:28px;
+    padding: 96px 0 16px;
+  }
+  .wx-pic{
+    width: 282px;
+    height: 282px;
+    background:rgba(216,216,216,1);
+    border:1px solid rgba(151,151,151,1);
+    margin: 0 auto;
+    .wx-img{
+      width: 282px;
+      height: 282px;
+    }
+  }
+  .tip{
+    margin-top: 30px;
+    img{
+      width: 283px;
+    }
+  }
 }
 
 </style>
